cd3b4522486296c791ac691305b6e168003cd6cf
Jan <janhu@gmx.net>
2025-04-02 17:29:08 +0200

    Init commit of installing tailwind, svelte, vite and daisyUI

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+</script>
+
+
+<main>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  
+  <div class="card">
+    <Counter />
+  </div>
+</main>
+
